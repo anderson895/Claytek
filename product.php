3af66e71b9654b91f937e71b0603d785ac8de509
JARP <andersonandy046@gmail.com>
2024-11-15 21:36:36 +0800

    first commit

---
+++ product.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Dashboard</title>
+  <link rel="stylesheet" href="css/testdashboard.css">
+</head>
+<body>
+
+  <!-- Sidebar -->
+  <div class="sidebar">
+    <div class="logo">
+      <a href="#" class="logoo">
+        <img src="images/LIGHTlogo.png" alt="Claytek Studios Logo">
+      </a>
+    </div>
+    <ul class="menu">
+      <li ><a href="#">Dashboard</a></li>
+      <li class="active"><a href="#">Products</a></li>
+      <li><a href="index.html">Website Homepage</a></li>
+      <li><a href="">Customer</a></li>
+      <li><a href="#">Orders</a></li>
+      <li><a href="#">Coupons</a></li>
+      <li><a href="#">Chats</a></li>
+      <li><a href="#">Settings</a></li>
+      <li><a href="#">Logout</a></li>
+    </ul>
+  </div>
+
+  <!-- Main Content -->
+  <div class="main-content">
+    <!-- Header -->
+    <div class="header">
+      <input type="search" placeholder="Search something here">
+      <div class="profile">
+        <span>Administrator</span>
+        <img src="images/admin.jpg" alt="Profile">
+      </div>
+    </div>
+
+    <!-- Dashboard Overview -->
+    
+    </div>
+  </div>
+
+</body>
+</html>
